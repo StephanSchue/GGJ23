cc74a1b217e6a6dbeed0df5991ed749f5eefff63
Stephan Schüritz <stephan.schueritz@gmail.com>
2023-02-04 15:59:03 +0000

    Add Interactable

--- 03 - Unity/Power GRRR/Assets/Scripts/Game/Interactable.cs
+++ 03 - Unity/Power GRRR/Assets/Scripts/Game/Interactable.cs
@@ -2,8 +2,51 @@ using UnityEngine;
 
 namespace GGJ23.Game
 {
+    public enum InteractionStatus
+    {
+        Free,
+        Progress,
+        Finished
+    }
+
     public class Interactable : MonoBehaviour
     {
+        public InteractionStatus _status;
+        public SpriteRenderer sprite;
+        public float _progress = 0f;
+        public float _duration = 2f;
+        public float _interactionRadius = 1f;
 
+        private void Awake()
+        {
+            Initalize();
+        }
+
+        public void Initalize()
+        {
+            _status = InteractionStatus.Free;
+            sprite.color = Color.red;
+            _progress = _duration;
+        }
+
+        public void Process(float dt)
+        {
+            if (_status < InteractionStatus.Finished)
+            {
+                if (_progress < 0f)
+                {
+                    sprite.color = Color.green;
+                    _status = InteractionStatus.Finished;
+                    return;
+                }
+
+                _progress -= dt;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, _interactionRadius);
+        }
     }
 }
--- 03 - Unity/Power GRRR/Assets/Scripts/Game/InteractionController.cs
+++ 03 - Unity/Power GRRR/Assets/Scripts/Game/InteractionController.cs
@@ -4,6 +4,45 @@ namespace GGJ23.Game
 {
     public class InteractionController : MonoBehaviour
     {
+        public float interactionRadius = 1f;
+        private Interactable[] _interactables;
+        private Interactable _nearestInteractable;
 
+        private void Awake()
+        {
+            _interactables = FindObjectsByType<Interactable>(FindObjectsSortMode.None);
+        }
+
+        private void Update()
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                // Check for nearest Interactable
+                float nearestDistance = float.MaxValue;
+                _nearestInteractable = null;
+
+                for (int i = 0; i < _interactables.Length; i++)
+                {
+                    float tmpDistance = Vector2.Distance(transform.position, _interactables[i].transform.position);
+
+                    if (tmpDistance < (interactionRadius + _interactables[i]._interactionRadius) && tmpDistance < nearestDistance)
+                    {
+                        _nearestInteractable = _interactables[i];
+                        nearestDistance = tmpDistance;
+                    }
+                }
+
+                // Interact
+                if (_nearestInteractable != null)
+                {
+                    _nearestInteractable.Process(Time.deltaTime);
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, interactionRadius);
+        }
     }
 }
