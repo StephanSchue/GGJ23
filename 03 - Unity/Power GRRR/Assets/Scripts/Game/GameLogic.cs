04d178459b14cbdc9d1554630d38588c1142f3eb
Stephan Schüritz <stephan.schueritz@gmail.com>
2023-02-04 17:27:08 +0000

    Update GameLogic

--- 03 - Unity/Power GRRR/Assets/Scripts/Game/GameLogic.cs
+++ 03 - Unity/Power GRRR/Assets/Scripts/Game/GameLogic.cs
@@ -10,7 +10,7 @@ namespace GGJ23.Game
         [SerializeField] private int _dayDurationMs = 60000;
         [SerializeField] private int _nightDurationMs = 10000;
 
-        private InteractionController _interactionController;
+        public InteractionController _interactionController;
         private bool _isNight = false;
         private float _currentScore = 0f;
         private float _currentTime = 1f; // 1 = first day, 1.5 = first night, 2 = second day etc.
@@ -20,17 +20,11 @@ namespace GGJ23.Game
         public UnityEvent OnNightSwitch;
 
         public bool IsNight => _isNight;
-
-        private void Awake()
-        {
-            _interactionController = new();
-            _interactionController.PopulateInteractables(FindObjectsByType<Interactable>(FindObjectsSortMode.None), OnDaySwitch, OnNightSwitch);
-        }
-
+        
         // Start is called before the first frame update
         void Start()
         {
-            
+            _interactionController.PopulateInteractables(FindObjectsByType<Interactable>(FindObjectsSortMode.None), OnDaySwitch, OnNightSwitch);
         }
 
         // Update is called once per frame
