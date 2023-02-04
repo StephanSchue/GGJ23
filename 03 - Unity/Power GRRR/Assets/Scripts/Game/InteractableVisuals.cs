ba5dcce448f8772d81cfa62596e5990b0e7fc39d
Stephan Schüritz <stephan.schueritz@gmail.com>
2023-02-04 20:59:12 +0000

    Update Interactable Animator

--- 03 - Unity/Power GRRR/Assets/Scripts/Game/InteractableVisuals.cs
+++ 03 - Unity/Power GRRR/Assets/Scripts/Game/InteractableVisuals.cs
@@ -8,13 +8,40 @@ namespace GGJ23.Game
         public SpriteRenderer spriteRenderer;
         public SpriteRenderer spriteLightGlow;
 
+        public Animator animator;
+        public RuntimeAnimatorController animatorController;
+
+        private bool animate = false;
+
         public Color[] statusColor;
+
+        private void Awake()
+        {
+            animator.runtimeAnimatorController = animatorController;
+            animate = animatorController != null;
+        }
 
         // Update is called once per frame
         private void Update()
         {
-            spriteRenderer.color = statusColor[(int)interactable.Status];
-            spriteLightGlow.enabled = interactable.IsNight;
+            var status = interactable.Status;
+            spriteRenderer.color = statusColor[(int)status];
+
+            bool working = (status == InteractionStatus.Working && status == InteractionStatus.FreshlyRepaired);
+
+            if (interactable.IsNight)
+            {
+                spriteLightGlow.enabled = working;
+            }
+            else
+            {
+                spriteLightGlow.enabled = false;
+            }
+
+            if (animate)
+            {
+                animator.SetBool("Broken", !working);
+            }
         }
     }
 }
