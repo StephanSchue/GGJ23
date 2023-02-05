df916fdc13dd6fd7ed7ea49be819ac03a4bf3029
Zylus <paulwulffmail@gmail.com>
2023-02-05 12:48:22 +0000

    Added placeholder notification for broken plants

--- 03 - Unity/Power GRRR/Assets/Scripts/Game/InteractableVisuals.cs
+++ 03 - Unity/Power GRRR/Assets/Scripts/Game/InteractableVisuals.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 namespace GGJ23.Game
 {
@@ -8,6 +9,7 @@ namespace GGJ23.Game
         public Interactable interactable;
         public SpriteRenderer spriteRenderer;
         public SpriteRenderer spriteRendererLightGlow;
+        public SpriteRenderer notification;
         public Animator animator;
 
         [Header("Settings")]
@@ -17,6 +19,8 @@ namespace GGJ23.Game
         public bool useAnimator = true;
 
         private bool animate = false;
+        private bool fadingOut = false;
+        private bool fadingIn = false;
 
         private void Awake()
         {
@@ -37,15 +41,55 @@ namespace GGJ23.Game
             if (interactable.IsNight)
             {
                 spriteRendererLightGlow.enabled = working;
+                notification.gameObject.SetActive(false);
+                fadingIn = false;
+                fadingOut = false;
+                notification.color = new Color(notification.color.r, notification.color.g, notification.color.b, 1f);
             }
             else
             {
                 spriteRendererLightGlow.enabled = false;
+                if (working)
+                {
+                    notification.gameObject.SetActive(false);
+                    fadingIn = false;
+                    fadingOut = false;
+                    notification.color = new Color(notification.color.r, notification.color.g, notification.color.b, 1f);
+                }
+                else
+                {
+                    notification.gameObject.SetActive(true);
+                    if (!fadingOut && !fadingIn)
+                    {
+                        fadingOut = true;
+                        DOTween.ToAlpha(() => notification.color, x => notification.color = x, 0f, 1f).onComplete = () => SwitchFade();
+                    }
+                }
             }
 
             if (animate && useAnimator)
             {
                 animator.SetBool("Broken", !working);
+            }
+        }
+
+        private void SwitchFade()
+        {
+            if (!fadingIn && !fadingOut)
+            { 
+                    notification.color = new Color(notification.color.r, notification.color.g, notification.color.b, 1f);
+                    return;
+            }
+
+            fadingIn = !fadingIn;
+            fadingOut = !fadingOut;
+            if (fadingIn)
+            {
+                        DOTween.ToAlpha(() => notification.color, x => notification.color = x, 1f, 1f).onComplete = () => SwitchFade();
+            }
+            else if (fadingOut)
+            {
+                        DOTween.ToAlpha(() => notification.color, x => notification.color = x, 0f, 1f).onComplete = () => SwitchFade();
             }
         }
     }
