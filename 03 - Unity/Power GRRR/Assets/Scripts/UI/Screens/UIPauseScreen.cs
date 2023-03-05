d9dd5a6ebb726dfb3607c6db38beaa6eefba408a
Stephan Schüritz <stephan.schueritz@gmail.com>
2023-03-05 16:03:49 +0000

    Fix ContentSizeFitter issue + Add new UI StartButton + Add new Controls Graphics

--- 03 - Unity/Power GRRR/Assets/Scripts/UI/Screens/UIPauseScreen.cs
+++ 03 - Unity/Power GRRR/Assets/Scripts/UI/Screens/UIPauseScreen.cs
@@ -1,10 +1,19 @@
 using GGJ23.Game;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GGJ23.UI
 {
     public class UIPauseScreen : UIScreen
     {
+        public ContentSizeFitter controlsGroup;
+
+        public override void Enter()
+        {
+            base.Enter();
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)controlsGroup.transform);
+        }
+
         public override void Tick(float dt, UIInputData inputData)
         {
             if (inputData.IsPressed(InputButton.Accept))
